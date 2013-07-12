aaac4a02e33be114cbe601975031630d8832ee6e
SunsparcSolaris <brandon@nomegahurts.com>
2013-07-11 23:35:18 -0400

    Recoded password change function for bcrypt.

--- chgpass.php
+++ chgpass.php
@@ -49,19 +49,22 @@ echo " <h3>$usr - Change Password</h3> ";
 /*Set up db connection*/
 require 'connect.php';
 
+require 'lib/password.php';
+
 /*Change POST variables to actual variables, escape the input*/
-$oldpass = md5(mysql_real_escape_string($_POST['oldpass']));
-$newpass = md5(mysql_real_escape_string($_POST['newpass']));
-$confnewpass = md5(mysql_real_escape_string($_POST['confnewpass']));
+$oldpass = mysql_real_escape_string($_POST['oldpass']);
+$newpass = mysql_real_escape_string($_POST['newpass']);
+$confnewpass = mysql_real_escape_string($_POST['confnewpass']);
 
 /*Select info from the database. usr must match session user and pass must match the provided oldpass*/
-$row = mysql_fetch_array(mysql_query("SELECT pass FROM login WHERE usr='{$_SESSION['usr']}' and pass='{$oldpass}'"));
+$row = mysql_fetch_array(mysql_query("SELECT pass FROM login WHERE usr='{$_SESSION['usr']}'"));
 
-if ( $oldpass == $row['pass'] && $newpass == $confnewpass && count($row) > 0 ) {
-mysql_query("UPDATE login SET pass = '{$newpass}' WHERE usr = '{$_SESSION['usr']}'");
+if ( password_verify($oldpass, $row['pass']) && $newpass == $confnewpass && count($row) > 0 ) {
+$hash = password_hash($newpass, PASSWORD_BCRYPT);
+mysql_query("UPDATE login SET pass = '{$hash}' WHERE usr = '{$_SESSION['usr']}'");
 echo " <center>Password Updated. You may return to the Portal Page.</center> ";
 }
-elseif ( $oldpass !== $row['pass'] && $_POST['chgpassbutton'] == "Change Password" ) {
+elseif ( !password_verify($oldpass, $row['pass']) && $_POST['chgpassbutton'] == "Change Password" ) {
 echo " <center>Old password does not match the database, try again!</center> ";
 }
 elseif ( $newpass !== $confnewpass && $_POST['chgpassbutton'] == "Change Password" ) {
