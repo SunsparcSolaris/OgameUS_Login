c25331ca9bd4aba9a722585b5c8b5b28766af252
SunsparcSolaris <brandon@nomegahurts.com>
2013-08-03 18:28:23 -0400

    Added section to structure new portal.php maindiv table.

--- ga.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'table.php';
-
-<center>Add New User: &nbsp;
-<form action="#" method="post">
-<input type="text" size="15" name="newuser" />
-<input type="submit" name="submit" value="Submit"
-</center>
-?>
