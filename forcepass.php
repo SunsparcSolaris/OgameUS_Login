9054ca23da3ba8d2bb740cfa8676c2e5a18b664c
SunsparcSolaris <brandon@nomegahurts.com>
2014-01-02 23:29:17 +0000

    Fixed forcepass page to work correctly.

--- forcepass.php
+++ forcepass.php
@@ -46,21 +46,24 @@ echo " <h3>$usr - Forced Password Change</h3> ";
 
 /*Set up db connection*/
 require 'connect.php';
+require 'lib/password.php';
 
 /*Change POST variables to actual variables, escape the input*/
-$oldpass = md5(mysql_real_escape_string($_POST['oldpass']));
-$newpass = md5(mysql_real_escape_string($_POST['newpass']));
-$confnewpass = md5(mysql_real_escape_string($_POST['confnewpass']));
+$oldpass = mysql_real_escape_string($_POST['oldpass']);
+$newpass = mysql_real_escape_string($_POST['newpass']);
+$confnewpass = mysql_real_escape_string($_POST['confnewpass']);
 
 /*Select info from the database. usr must match session user and pass must match the provided oldpass*/
-$row = mysql_fetch_array(mysql_query("SELECT pass FROM login WHERE usr='{$_SESSION['usr']}' and pass='{$oldpass}'"));
+$row = mysql_fetch_array(mysql_query("SELECT pass FROM login WHERE usr='{$_SESSION['usr']}'"));
+$dbpass = $row['pass'];
 
-if ( $oldpass == $row['pass'] && $newpass == $confnewpass && count($row) > 0 ) {
+if ( password_verify($oldpass, $dbpass) && $newpass == $confnewpass && count($row) > 0 ) {
+$newpass = password_hash($newpass, PASSWORD_BCRYPT);
 mysql_query("UPDATE login SET pass = '{$newpass}', forcepass = 0 WHERE usr = '{$_SESSION['usr']}'");
 echo " <center>Password Updated. Redirecting to the Portal Page.</center> ";
-header('Location: portal.php');
+echo "<meta http-equiv='refresh' content='3,url=portal.php'>";
 }
-elseif ( $oldpass !== $row['pass'] && $_POST['chgpassbutton'] == "Change Password" ) {
+elseif ( !password_verify($oldpass, $dbpass) && $_POST['chgpassbutton'] == "Change Password" ) {
 echo " <center>Old password does not match the database, try again!</center> ";
 }
 elseif ( $newpass !== $confnewpass && $_POST['chgpassbutton'] == "Change Password" ) {
