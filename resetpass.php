169c9a3453864c84cafa3ded08a4693d55610450
SunsparcSolaris <brandon@nomegahurts.com>
2014-12-05 18:00:17 -0500

    Finshed fleshing out reset logic.

--- resetpass.php
+++ resetpass.php
@@ -13,7 +13,8 @@
 <form action="#" method="post">
 <p>Please enter the email address associated with your Ogame.US Portal account.</p>
 <label>Email Address:</label> <input type="text" size="24" name="email" /><br /><br />
-<input type="submit" name="resetbutton" value="Reset Password" />
+<input type="submit" name="submitbutton" value="Reset Password" />
+<input type="hidden" name="resetbutton" value="resetbutton" />
 </form>
 </center>
 </div>
@@ -29,16 +30,24 @@ require 'lib/password.php';
 require 'rdmpwgen.php';
 
 $email = strtolower($_POST['email']);
+$actiontype = $_POST['resetbutton'];
+
+if ($actiontype == "resetbutton") {
 
 $row = mysql_fetch_array(mysql_query("SELECT usr,email FROM login WHERE email='{$email}'"));
-
+$usr = $row['usr'];
 if ( $email == $row['email'] && count($row) > 0 ) {
 $rdmpass = rand_string(5);
 $newpass = password_hash($rdmpass, PASSWORD_BCRYPT);
 mysql_query("UPDATE login SET pass = '{$newpass}', forcepass = 1 WHERE email = '{$email}'");
 echo "<br /><br /><center>Please check your Ogame.US email for more instructions.</center> ";
-mail($email,"Ogame.US Portal Password Reset","Hello, $usr, \r\n  Password: $rdmpass","From: PinkFloyd@ogame.us");
+mail($email,"Ogame.US Portal Password Reset","Hello, $usr, \r\n Your Ogame.US Portal Account Password has been reset. \r\n \r\n New Password: $rdmpass \r\n Regards","From: PinkFloyd@ogame.us");
 }
 
 mysql_close($con);
+}
+else {
+echo "";
+}
+
 ?>
