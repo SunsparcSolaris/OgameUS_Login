25a91be94fb37acf854a76a7e096d0fe2a61eea7
SunsparcSolaris <brandon@nomegahurts.com>
2015-05-27 11:35:27 -0400

    Added styling to logout landing page.

--- logout.php
+++ logout.php
@@ -3,7 +3,8 @@ session_start();
 unset($_SESSION);
 session_destroy();
 session_write_close();
-print " <center><h1>Logged out!</h1><br /> <h3>Redirecting to Login page...</h3></center> ";
-        echo "<meta http-equiv='refresh' content='2,url=index.php'>";
+echo "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"design.css\" /></head>
+<br /><br /><br /><br /><body class=\"portal\"><center><h1>Logged out!</h1><br /> <h3>Redirecting to Login page...</h3></center></body></html> ";
+        echo "<meta http-equiv='refresh' content='4,url=index.php'>";
 die;
 ?>
